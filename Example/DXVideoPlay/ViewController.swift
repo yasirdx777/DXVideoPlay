17f9d980a44f709baa4e62038e065602e7a7c0d7
Yasir Romaya <yasir.romaya@gmail.com>
2022-04-05 17:17:58 +0300

    update the example

--- Example/DXVideoPlay/ViewController.swift
+++ Example/DXVideoPlay/ViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
+import DXVideoPlay
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        
+        let videoMP4Source480p = VideoSource(sourceTitel: "480p", sourceVideo: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!)
+        let videoMP4Source720p = VideoSource(sourceTitel: "720p", sourceVideo: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!)
+        let subtitleSRTSource = URL(string: "https://raw.githubusercontent.com/nick-vanpraet/subtitles-test/master/D20/FHSY/e01.srt")!
+        
+        
+        let assetItemOne = AssetItem(id: 0, itemTitle: "Clip 1", itemSubtitle: subtitleSRTSource, itemVideoSources: [videoMP4Source480p, videoMP4Source720p])
+        let assetItemTwo = AssetItem(id: 0, itemTitle: "Clip 1", itemSubtitle: subtitleSRTSource, itemVideoSources: [videoMP4Source480p, videoMP4Source720p])
+        
+        
+        let model = DXPlayerModel(id: 101, assetItems: [assetItemOne, assetItemTwo])
+        let dxPlayer = DXVideoPlay(playerModel: model)
+        dxPlayer.modalPresentationStyle = .fullScreen
+        
+        present(dxPlayer, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
-
